1ec715f7b1fafcf693bb4121e01cf4d7547712ba
Sudhir Nayak <sudhir@knowledgeworksindia.com>
2021-05-26 18:54:16 +0530

    completed code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 
 namespace SampleConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            var counter=0; 
+            var max = args.Length==0? -1: Convert.ToInt32(args[0]);
+            while(max == -1 || counter < max){
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
+            }
         }
     }
 }
